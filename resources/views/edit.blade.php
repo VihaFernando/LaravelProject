58c24d551a1e8680e957133651da33c2a8b30d62
VihaFernando <vihanga.20220051@gmail.com>
2024-08-01 11:51:32 +0530

    creates the products edit page

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Edit Product</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('products.update', $product) }}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="name">Product Name</label>
+            <input type="text" name="name" class="form-control" value="{{ old('name', $product->name) }}" required>
+        </div>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="number" name="price" class="form-control" value="{{ old('price', $product->price) }}" required>
+        </div>
+        <!-- Add more fields as necessary -->
+        <button type="submit" class="btn btn-primary">Update Product</button>
+    </form>
+@endsection
