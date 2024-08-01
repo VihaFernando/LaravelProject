ed6c13be48508ef331a7c4550d2886b8f9f0f66d
VihaFernando <vihanga.20220051@gmail.com>
2024-08-01 14:54:52 +0530

    updates the routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 
 // Redirect unauthenticated users to login or authenticated users to admin dashboard
 Route::get('/', function () {
@@ -24,6 +25,12 @@ Route::get('/admin', function () {
     return view('admin');
 })->middleware('auth');
 
-Route::get('/products', function () {
-    return view('createproducts');
+// Product routes with authentication middleware
+Route::middleware('auth')->group(function () {
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index'); // List products
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create'); // Show form to add new product
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // Store new product
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit'); // Show form to edit product
+    Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update'); // Update product
+    Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy'); // Delete product
 });
