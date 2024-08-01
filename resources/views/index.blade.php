b8a34e7db779fb98a8334e47fcf837132a7883aa
VihaFernando <vihanga.20220051@gmail.com>
2024-08-01 11:39:49 +0530

    creates the product view page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Product List</h1>
+
+    <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">Add Product</a>
+
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+                <tr>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ $product->price }}</td>
+                    <td>
+                        <a href="{{ route('products.show', $product) }}" class="btn btn-info">View</a>
+                        <a href="{{ route('products.edit', $product) }}" class="btn btn-warning">Edit</a>
+                        <form action="{{ route('products.destroy', $product) }}" method="POST" style="display: inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
