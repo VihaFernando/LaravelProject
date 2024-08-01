c522dc44d4345914ee7dabc7044875d1c9d36c39
VihaFernando <vihanga.20220051@gmail.com>
2024-08-01 14:42:47 +0530

    updates the views

--- resources/views/productslist.blade.php
+++ resources/views/productslist.blade.php
@@ -2,13 +2,21 @@
 
 @section('content')
     <h1>Product List</h1>
+
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
+    <a href="{{ route('products.create') }}" class="btn btn-primary">Add New Product</a>
+
     <table>
         <thead>
             <tr>
                 <th>Name</th>
                 <th>Price</th>
                 <th>Description</th>
-                <th>Image</th>
                 <th>Actions</th>
             </tr>
         </thead>
@@ -18,13 +26,12 @@
                     <td>{{ $product->name }}</td>
                     <td>${{ $product->price }}</td>
                     <td>{{ $product->description }}</td>
-                    <td><img src="{{ $product->image }}" alt="{{ $product->name }}" width="100"></td>
                     <td>
-                        <a href="{{ route('products.edit', $product->id) }}">Edit</a>
+                        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning">Edit</a>
                         <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit">Delete</button>
+                            <button type="submit" class="btn btn-danger">Delete</button>
                         </form>
                     </td>
                 </tr>
--- resources/views/updateproducts.blade.php
+++ resources/views/updateproducts.blade.php
@@ -2,29 +2,19 @@
 
 @section('content')
     <h1>Edit Product</h1>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-
-    <form action="{{ route('products.update', $product) }}" method="POST">
+    <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
-        <div class="form-group">
-            <label for="name">Product Name</label>
-            <input type="text" name="name" class="form-control" value="{{ old('name', $product->name) }}" required>
-        </div>
-        <div class="form-group">
-            <label for="price">Price</label>
-            <input type="number" name="price" class="form-control" value="{{ old('price', $product->price) }}" required>
-        </div>
-        <!-- Add more fields as necessary -->
-        <button type="submit" class="btn btn-primary">Update Product</button>
+        
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="{{ old('name', $product->name) }}" required>
+        
+        <label for="price">Price:</label>
+        <input type="text" id="price" name="price" value="{{ old('price', $product->price) }}" required>
+        
+        <label for="description">Description:</label>
+        <textarea id="description" name="description">{{ old('description', $product->description) }}</textarea>
+        
+        <button type="submit">Update Product</button>
     </form>
 @endsection
